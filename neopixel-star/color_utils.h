cc1eacc370e6bd0309fb10c1cd3aaf774cbc21c8
Martin Strauss <martin@ockle.org>
2020-12-17 19:11:41 -0800

    delete some unused code

--- neopixel-star/color_utils.h
+++ neopixel-star/color_utils.h
@@ -1,18 +1,11 @@
 // From https://gist.github.com/postspectacular/2a4a8db092011c6743a7
+
 // HSV->RGB conversion based on GLSL version
 // expects hsv channels defined in 0.0 .. 1.0 interval
-float fract(float x) { return x - int(x); }
 
 float mix(float a, float b, float t) { return a + (b - a) * t; }
 
 float step(float e, float x) { return x < e ? 0.0 : 1.0; }
-
-float* hsv2rgb(float h, float s, float b, float* rgb) {
-  rgb[0] = b * mix(1.0, constrain(abs(fract(h + 1.0) * 6.0 - 3.0) - 1.0, 0.0, 1.0), s);
-  rgb[1] = b * mix(1.0, constrain(abs(fract(h + 0.6666666) * 6.0 - 3.0) - 1.0, 0.0, 1.0), s);
-  rgb[2] = b * mix(1.0, constrain(abs(fract(h + 0.3333333) * 6.0 - 3.0) - 1.0, 0.0, 1.0), s);
-  return rgb;
-}
 
 float* rgb2hsv(float r, float g, float b, float* hsv) {
   float s = step(b, g);
@@ -33,20 +26,17 @@ float* rgb2hsv(float r, float g, float b, float* hsv) {
 
 // From https://learn.adafruit.com/multi-tasking-the-arduino-part-3/utility-functions
 
-    // Returns the Red component of a 32-bit color
-    uint8_t Red(uint32_t color)
-    {
-        return (color >> 16) & 0xFF;
-    }
- 
-    // Returns the Green component of a 32-bit color
-    uint8_t Green(uint32_t color)
-    {
-        return (color >> 8) & 0xFF;
-    }
- 
-    // Returns the Blue component of a 32-bit color
-    uint8_t Blue(uint32_t color)
-    {
-        return color & 0xFF;
-    }
+// Returns the Red component of a 32-bit color
+uint8_t Red(uint32_t color) {
+  return (color >> 16) & 0xFF;
+}
+
+// Returns the Green component of a 32-bit color
+uint8_t Green(uint32_t color) {
+  return (color >> 8) & 0xFF;
+}
+
+// Returns the Blue component of a 32-bit color
+uint8_t Blue(uint32_t color) {
+  return color & 0xFF;
+}
